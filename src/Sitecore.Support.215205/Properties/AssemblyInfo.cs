ff600f8a0627c28147e27c1ac4db3c9081f342b4
Ivan Sheyenko <ivs@sitecore.net>
2018-10-17 12:49:14 -0400

    fix - set EXM dimensions typr to NopDimension if EXM is disabled

---
+++ src/Sitecore.Support.215205/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.215205")]
+[assembly: AssemblyProduct("Sitecore.Support.215205")]
+[assembly: ComVisible(false)]
